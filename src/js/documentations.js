9629d052ae3d7968eac628b71812dffbe7591a6c
saulojustiniano1 <saulojustiniano1@gmail.com>
2024-10-20 23:57:24 -0300

    Refactor toggleAccordion function to use 'rem' units for maxHeight

--- src/js/documentations.js
+++ src/js/documentations.js
@@ -1,11 +1,9 @@
 function toggleAccordion() {
   const content = document.getElementById('accordionContent');
 
-  // Se já estiver expandido, colapsa
   if (content.style.maxHeight) {
     content.style.maxHeight = null;
   } else {
-    // Expande o conteúdo
-    content.style.maxHeight = content.scrollHeight + 'px';
+    content.style.maxHeight = content.scrollHeight + 'rem';
   }
 }
